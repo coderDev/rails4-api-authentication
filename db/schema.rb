7c7b4280aa1a896ad1a71b5efcbc0e02a93d7d6a
arnavdots <arnav.jain@dotsquares.com>
2014-12-03 11:20:21 +0530

    After rake db:migrate (authentication_token)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203053645) do
+ActiveRecord::Schema.define(version: 20141203054150) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20141203053645) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
