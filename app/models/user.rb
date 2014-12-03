6585bdbdcf107f9b56da084e770b6f319cad47e7
arnavdots <arnav.jain@dotsquares.com>
2014-12-03 17:26:10 +0530

    Added Rooms controller & views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  has_many :rooms
 end
