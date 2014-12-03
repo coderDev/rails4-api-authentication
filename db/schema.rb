8b4f35391b3b253f875cc5e75f91f9f6f55109d6
arnavdots <arnav.jain@dotsquares.com>
2014-12-03 15:33:08 +0530

    Added Room model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203054150) do
+ActiveRecord::Schema.define(version: 20141203095406) do
+
+  create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rooms", ["user_id"], name: "index_rooms_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
